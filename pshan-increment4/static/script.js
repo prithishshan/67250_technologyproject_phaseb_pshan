fe235f601e66551c12814fedc37afb5fbefa92f4
prithishshan <prithish.shan@gmail.com>
2026-04-05 22:45:26 -0400

    inc 4

---
+++ pshan-increment4/static/script.js
@@ -0,0 +1,100 @@
+const x = 2;
+const y = 10;
+console.log(x + y);
+{
+    const x = "hello";
+    const y = "world";
+    console.log(x + " " + y);
+}
+
+function sumArgs(a,b) {
+    console.log(a + b);
+}
+
+sumArgs(2, 10);
+
+sumArgs("hi", " everyone");
+const C = "my string";
+const z = 9;
+
+if (C.length >= z) {
+    if (C.length == z) {
+        console.log("good job!");
+    } else {
+        console.log("C");
+    }
+} else
+    console.log("z");
+
+L1 = ["Watermelon","Pineapple","Pear","Banana"];
+L2 = ["Apple","Banana","Kiwi","Orange"];
+
+function checkBanana(list) {
+    list.forEach((x) => {
+        if (x == "Banana") alert(x);
+    });
+}
+
+// checkBanana(L1);
+
+// checkBanana(L2);
+
+function greeting() {
+    const currtime = new Date()
+    const greetEl = document.getElementById("greeting");
+    if (greetEl) {
+        if (currtime.getHours() < 5) {
+            greetEl.innerHTML = "Good Night!"
+        } else if (currtime.getHours() < 12) {
+            greetEl.innerHTML = "Good Morning!"
+        } else if (currtime.getHours() < 18){
+            greetEl.innerHTML = "Good Afternoon!"
+        } else if (currtime.getHours() < 20){
+            greetEl.innerHTML = "Good Evening!"
+        } else {
+            greetEl.innerHTML = "Good Night!"
+        }
+    }
+    console.log(currtime)
+}
+
+greeting()
+
+function setYear() {
+    const footerYear = document.getElementById("copyYear")
+    footerYear.innerHTML = `&copy; ${(new Date()).getFullYear()} MonoMuse. All rights reserved.`
+}
+
+function setActive() {
+    const navLinks = document.querySelectorAll("nav a");
+    
+    navLinks.forEach((link) => {
+        console.log(link.href);
+        if (link.href === window.location.href) {
+            link.id = "selected";
+        } else {
+            link.id = "";
+        }    
+    });
+}
+setActive();
+
+$("#readLess").click(function(){ 
+    $("#short-desc").show(); 
+    $("#full-desc").hide(); 
+
+  });
+  
+$("#readMore").click(function(){
+    $("#full-desc").show();
+    $("#short-desc").hide(); 
+});
+
+$("#selectTickets").click(function(){
+    $("#checkoutForm").show();
+    $("#selectTickets").hide();
+});
+
+function purchase() {
+    alert("Thank you for your purchase! We look forward to seeing you at MonoMuse.");
+}
